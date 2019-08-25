dc7a0fe023da86c4859bca08c6e453cb47d181a5
etim uwem <soughtoutonlyme@gmail.com>
2019-08-25 12:18:09 +0100

    added appveyor file for Continuous Integration, CI server

--- src/index.test.js
+++ src/index.test.js
@@ -13,13 +13,13 @@ describe('My first test', () => {
 })
 
 describe("index.html", () => {   //index.html is the file that we want to test
-  it("", (done) => {
-    const index = fs.readFileSync("./src/index.html", "utf-8");
-    jsdom.env(index, function(err, window){
-      const h1 = window.document.getElementsByTagName("h1")[0];
+  it("should say hello", (done) => {      //this is shoing tht i am writing a test to show that Hello is in the index.html file
+    const index = fs.readFileSync("./src/index.html", "utf-8");    //gettin s reference to the index.html file and holding it in memory via fs.readFileSync
+    jsdom.env(index, function(err, window){         //defining the jsdom environment and pass the index.html file. Add call-back function that is run after jsdom finishes pulling index.html into memory. Window here is same as window in browsers
+      const h1 = window.document.getElementsByTagName("h1")[0]; //getting the first h1 in an array(in the event there are more than one item in the array)
       expect(h1.innerHTML).to.equal("Hello Nigeria");
       done();
-      window.close();
+      window.close(); //closing the window to free up the memory that was taken when the in-memory DOM was created.
     })
   })
 })
